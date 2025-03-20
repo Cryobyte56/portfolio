5b88234de0bc68cb29692ea73dca8ed48eb67ba5
Cryobyte56 <danielasanon54@gmail.com>
2025-03-20 21:22:05 +0800

    Fix Vite build output

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/",
   build: {
-    outDir: "dist", 
-    emptyOutDir: true, 
+    outDir: "dist",
+    emptyOutDir: true,
+    assetsDir: "javascripts",
   },
 });
