e8e4dc61c0732d07e29a18824d5829398275c19a
cryobyte56 <danielasanon54@gmail.com>
2025-03-20 12:56:16 +0800

    Added Maintenance Display

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#8cff00",
+        primary: "#58e61b",
       },
       animation: {
         text: "text 2s ease infinite",
+        dimlight: "dimlight 5s infinite",
       },
       keyframes: {
         text: {
@@ -21,9 +22,28 @@ export default {
             "background-position": "right center",
           },
         },
+
+        dimlight: {
+          "0%, 18%, 20%, 50.1%, 60%, 65.1%, 80%, 90.1%, 92%": {
+            color: "red",
+            boxShadow: "none",
+          },
+          "18.1%, 20.1%, 30%, 50%, 60.1%, 65%, 80.1%, 90%, 92.1%, 100%": {
+            color: "#fff",
+            textShadow: "0 0 10px #03bcf4",
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".box-reflect": {
+          "-webkit-box-reflect":
+            "below 1px linear-gradient(transparent, #0004)",
+        },
+      });
+    },
+  ],
 };
-
